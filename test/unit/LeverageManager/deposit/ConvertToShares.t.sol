2d3bd8da6108f2e7a15b95ad3d9934d732ccf183
kitanovicd <kitanovic.danilo@outlook.com>
2024-12-27 23:12:54 +0100

    fix: explicit rounding test

--- test/unit/LeverageManager/deposit/ConvertToShares.t.sol
+++ test/unit/LeverageManager/deposit/ConvertToShares.t.sol
@@ -18,6 +18,19 @@ contract ConvertToShares is LeverageManagerBaseTest {
         super.setUp();
     }
 
+    function test_convertToShares_RoundedDown() public {
+        uint128 equity = 1;
+        uint128 totalEquity = 100;
+        uint128 sharesTotalSupply = 99;
+
+        _mockState_ConvertToShareOrEquity(
+            ConvertToSharesState({totalEquity: totalEquity, sharesTotalSupply: sharesTotalSupply})
+        );
+
+        uint256 shares = leverageManager.exposed_convertToShares(strategy, equity);
+        assertEq(shares, 0);
+    }
+
     function testFuzz_convertToShares(uint128 equity, uint128 totalEquity, uint128 sharesTotalSupply) public {
         _mockState_ConvertToShareOrEquity(
             ConvertToSharesState({totalEquity: totalEquity, sharesTotalSupply: sharesTotalSupply})
