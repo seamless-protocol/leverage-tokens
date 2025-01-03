d5aeb437aaac6a1001673320c4143ce15adc385b
Wesley Frederickson <6827305+fredwes@users.noreply.github.com>
2025-01-03 08:32:35 -0800

    chore: update pragma and foundry.toml

--- test/unit/FeeManager/ChargeStrategyFee.t.sol
+++ test/unit/FeeManager/ChargeStrategyFee.t.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.26;
 
 // Dependency imports
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
--- test/unit/FeeManager/SetStrategyActionFee.sol
+++ test/unit/FeeManager/SetStrategyActionFee.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.26;
 
 // Dependency imports
 import {IAccessControl} from "@openzeppelin/contracts/access/IAccessControl.sol";
