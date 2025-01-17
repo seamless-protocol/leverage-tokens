62264a786fe8f723d3627212c43b9e13ba3f4134
chad-js <chadflemies@gmail.com>
2025-01-17 10:51:41 -0500

    test: fuzzing for exchange rate funcs

--- test/unit/LendingAdapter/morpho/ConvertCollateralToDebtAsset.t.sol
+++ test/unit/LendingAdapter/morpho/ConvertCollateralToDebtAsset.t.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.26;
 // Dependency imports
 import {IOracle} from "@morpho-blue/interfaces/IOracle.sol";
 import {ORACLE_PRICE_SCALE} from "@morpho-blue/libraries/ConstantsLib.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 // Internal imports
 import {MorphoLendingAdapterBaseTest} from "./MorphoLendingAdapterBase.t.sol";
 
 contract ConvertCollateralToDebtAsset is MorphoLendingAdapterBaseTest {
-    function test_convertCollateralToDebtAsset_RoundsDown() public {
+    /// @dev uint128 is used to avoid overflows in the test. Also, Morpho only supports up to type(uint128).max for debt and collateral
+    function testFuzz_convertCollateralToDebtAsset_RoundsDown(uint128 collateral) public {
         // Mock the price of the collateral asset in the debt asset to be 1 less than the scaling factor of Morpho oracles
         vm.mockCall(
             address(defaultMarketParams.oracle),
@@ -18,9 +20,11 @@ contract ConvertCollateralToDebtAsset is MorphoLendingAdapterBaseTest {
         );
 
         // debt = collateral * collateralAssetPriceInDebtAsset / ORACLE_PRICE_SCALE
-        // = 10 * (ORACLE_PRICE_SCALE - 1) / ORACLE_PRICE_SCALE
-        // = 10 * (1e36 - 1) / 1e36
-        // = 9 if rounded down
-        assertEq(lendingAdapter.convertCollateralToDebtAsset(10), 9);
+        // = collateral * (ORACLE_PRICE_SCALE - 1) / ORACLE_PRICE_SCALE
+        // = collateral * (1e36 - 1) / 1e36
+        assertEq(
+            lendingAdapter.convertCollateralToDebtAsset(collateral),
+            Math.mulDiv(collateral, ORACLE_PRICE_SCALE - 1, ORACLE_PRICE_SCALE, Math.Rounding.Floor)
+        );
     }
 }
--- test/unit/LendingAdapter/morpho/ConvertDebtToCollateralAsset.t.sol
+++ test/unit/LendingAdapter/morpho/ConvertDebtToCollateralAsset.t.sol
@@ -4,12 +4,14 @@ pragma solidity ^0.8.26;
 // Dependency imports
 import {IOracle} from "@morpho-blue/interfaces/IOracle.sol";
 import {ORACLE_PRICE_SCALE} from "@morpho-blue/libraries/ConstantsLib.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 // Internal imports
 import {MorphoLendingAdapterBaseTest} from "./MorphoLendingAdapterBase.t.sol";
 
 contract ConvertDebtToCollateralAsset is MorphoLendingAdapterBaseTest {
-    function test_convertDebtToCollateralAsset_RoundsUp() public {
+    /// @dev uint128 is used to avoid overflows in the test. Also, Morpho only supports up to type(uint128).max for debt and collateral
+    function testFuzz_convertDebtToCollateralAsset_RoundsUp(uint128 debt) public {
         // Mock the price of the collateral asset in the debt asset to be 1 less than the scaling factor of Morpho oracles
         vm.mockCall(
             address(defaultMarketParams.oracle),
@@ -18,9 +20,11 @@ contract ConvertDebtToCollateralAsset is MorphoLendingAdapterBaseTest {
         );
 
         // collateral = debt * ORACLE_PRICE_SCALE / collateralAssetPriceInDebtAsset
-        // = 10 * ORACLE_PRICE_SCALE / (ORACLE_PRICE_SCALE - 1)
-        // = 10 * 1e36 / (1e36 - 1)
-        // = 11 if rounded up
-        assertEq(lendingAdapter.convertDebtToCollateralAsset(10), 11);
+        // = debt * ORACLE_PRICE_SCALE / (ORACLE_PRICE_SCALE - 1)
+        // = debt * 1e36 / (1e36 - 1)
+        assertEq(
+            lendingAdapter.convertDebtToCollateralAsset(debt),
+            Math.mulDiv(debt, ORACLE_PRICE_SCALE, ORACLE_PRICE_SCALE - 1, Math.Rounding.Ceil)
+        );
     }
 }
