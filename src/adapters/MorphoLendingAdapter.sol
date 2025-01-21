df221c68811438f5159a1a2d9fc1b6e46af7e8dd
chad-js <chadflemies@gmail.com>
2025-01-20 19:23:11 -0500

    refactor: remove return var names from MorphoLendingAdapter contract

--- src/adapters/MorphoLendingAdapter.sol
+++ src/adapters/MorphoLendingAdapter.sol
@@ -62,22 +62,22 @@ contract MorphoLendingAdapter is IMorphoLendingAdapter, Initializable {
     }
 
     /// @inheritdoc ILendingAdapter
-    function getCollateralAsset() external view returns (IERC20 collateralAsset) {
+    function getCollateralAsset() external view returns (IERC20) {
         return IERC20(marketParams.collateralToken);
     }
 
     /// @inheritdoc ILendingAdapter
-    function getDebtAsset() external view returns (IERC20 debtAsset) {
+    function getDebtAsset() external view returns (IERC20) {
         return IERC20(marketParams.loanToken);
     }
 
     /// @inheritdoc ILendingAdapter
-    function convertCollateralToDebtAsset(uint256 collateral) public view returns (uint256 debt) {
+    function convertCollateralToDebtAsset(uint256 collateral) public view returns (uint256) {
         // Morpho oracles return the price of 1 asset of collateral token quoted in 1 asset of loan token, scaled by ORACLE_PRICE_SCALE.
         // More specifically, the price is quoted in `ORACLE_PRICE_SCALE + loan token decimals - collateral token decimals` decimals of precision.
         uint256 collateralAssetPriceInDebtAsset = IOracle(marketParams.oracle).price();
 
-        debt = _multiplyAndScale(
+        return _multiplyAndScale(
             collateral,
             collateralAssetPriceInDebtAsset,
             ORACLE_PRICE_SCALE,
@@ -88,12 +88,12 @@ contract MorphoLendingAdapter is IMorphoLendingAdapter, Initializable {
     }
 
     /// @inheritdoc ILendingAdapter
-    function convertDebtToCollateralAsset(uint256 debt) external view returns (uint256 collateral) {
+    function convertDebtToCollateralAsset(uint256 debt) external view returns (uint256) {
         // Morpho oracles return the price of 1 asset of collateral token quoted in 1 asset of loan token, scaled by ORACLE_PRICE_SCALE.
         // More specifically, the price is quoted in `ORACLE_PRICE_SCALE + loan token decimals - collateral token decimals` decimals of precision.
         uint256 collateralAssetPriceInDebtAsset = IOracle(marketParams.oracle).price();
 
-        collateral = _multiplyAndScale(
+        return _multiplyAndScale(
             debt,
             ORACLE_PRICE_SCALE,
             collateralAssetPriceInDebtAsset,
@@ -104,26 +104,26 @@ contract MorphoLendingAdapter is IMorphoLendingAdapter, Initializable {
     }
 
     /// @inheritdoc ILendingAdapter
-    function getCollateral() public view returns (uint256 collateral) {
-        collateral = MorphoLib.collateral(morpho, morphoMarketId, address(this));
+    function getCollateral() public view returns (uint256) {
+        return MorphoLib.collateral(morpho, morphoMarketId, address(this));
     }
 
     /// @inheritdoc ILendingAdapter
-    function getCollateralInDebtAsset() public view returns (uint256 collateral) {
+    function getCollateralInDebtAsset() public view returns (uint256) {
         return convertCollateralToDebtAsset(getCollateral());
     }
 
     /// @inheritdoc ILendingAdapter
-    function getDebt() public view returns (uint256 debt) {
+    function getDebt() public view returns (uint256) {
         return MorphoBalancesLib.expectedBorrowAssets(morpho, marketParams, address(this));
     }
 
     /// @inheritdoc ILendingAdapter
-    function getEquityInDebtAsset() external view returns (uint256 equity) {
+    function getEquityInDebtAsset() external view returns (uint256) {
         uint256 collateralInDebtAsset = getCollateralInDebtAsset();
         uint256 debt = getDebt();
 
-        equity = collateralInDebtAsset > debt ? collateralInDebtAsset - debt : 0;
+        return collateralInDebtAsset > debt ? collateralInDebtAsset - debt : 0;
     }
 
     /// @inheritdoc ILendingAdapter
@@ -174,18 +174,18 @@ contract MorphoLendingAdapter is IMorphoLendingAdapter, Initializable {
         uint256 inputDecimals,
         uint256 outputDecimals,
         Math.Rounding rounding
-    ) internal pure returns (uint256 scaledAmount) {
+    ) internal pure returns (uint256) {
         if (inputDecimals > outputDecimals) {
             // Scale down the input value
             uint256 scalingFactor = 10 ** (inputDecimals - outputDecimals);
-            scaledAmount = Math.mulDiv(value, numerator, denominator * scalingFactor, rounding);
+            return Math.mulDiv(value, numerator, denominator * scalingFactor, rounding);
         } else if (inputDecimals < outputDecimals) {
             // Scale up the input value
             uint256 scalingFactor = 10 ** (outputDecimals - inputDecimals);
-            scaledAmount = Math.mulDiv(value * scalingFactor, numerator, denominator, rounding);
+            return Math.mulDiv(value * scalingFactor, numerator, denominator, rounding);
         } else {
             // No scaling needed
-            scaledAmount = Math.mulDiv(value, numerator, denominator, rounding);
+            return Math.mulDiv(value, numerator, denominator, rounding);
         }
     }
 }
