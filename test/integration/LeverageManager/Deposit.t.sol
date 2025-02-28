6d1cd2291c95b6984272e75dca4f0d658f29cd07
Danilo Kitanovic <kitanovic.danilo@outlook.com>
2025-02-28 13:52:46 +0100

    feat: integration tests for LeverageManager withdraw

--- test/integration/LeverageManager/Deposit.t.sol
+++ test/integration/LeverageManager/Deposit.t.sol
@@ -2,82 +2,16 @@
 pragma solidity ^0.8.26;
 
 // Dependency imports
-import {UnsafeUpgrades} from "@foundry-upgrades/Upgrades.sol";
 import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
 
 // Dependency imports
-import {Id, IMorpho, MarketParams} from "@morpho-blue/interfaces/IMorpho.sol";
 import {IOracle} from "@morpho-blue/interfaces/IOracle.sol";
 
 // Internal imports
-import {IStrategy} from "src/interfaces/IStrategy.sol";
-import {CollateralRatios, StrategyState, ExternalAction} from "src/types/DataTypes.sol";
-import {LeverageManagerStorage as Storage} from "src/storage/LeverageManagerStorage.sol";
-import {LeverageManager} from "src/LeverageManager.sol";
-import {BeaconProxyFactory} from "src/BeaconProxyFactory.sol";
-import {Strategy} from "src/Strategy.sol";
-import {LeverageManagerHarness} from "test/unit/LeverageManager/harness/LeverageManagerHarness.t.sol";
-import {MorphoLendingAdapterTest} from "./MorphoLendingAdapter.t.sol";
-import {IRebalanceRewardDistributor} from "src/interfaces/IRebalanceRewardDistributor.sol";
-import {IRebalanceWhitelist} from "src/interfaces/IRebalanceWhitelist.sol";
-import {ILeverageManager} from "src/interfaces/ILeverageManager.sol";
-import {ILendingAdapter} from "src/interfaces/ILendingAdapter.sol";
-import {IntegrationTestBase} from "./IntegrationTestBase.t.sol";
+import {LeverageManagerBase} from "./LeverageManagerBase.t.sol";
+import {StrategyState, CollateralRatios, ExternalAction} from "src/types/DataTypes.sol";
 
-contract LeverageManagerTest is IntegrationTestBase {
-    uint256 public BASE_RATIO;
-    address public user = makeAddr("user");
-    IStrategy public strategy;
-
-    function setUp() public virtual override {
-        address leverageManagerImplementation = address(new LeverageManagerHarness());
-        leverageManager = ILeverageManager(
-            UnsafeUpgrades.deployUUPSProxy(
-                leverageManagerImplementation,
-                abi.encodeWithSelector(LeverageManager.initialize.selector, address(this))
-            )
-        );
-        LeverageManager(address(leverageManager)).grantRole(keccak256("FEE_MANAGER_ROLE"), address(this));
-
-        super.setUp();
-
-        BASE_RATIO = LeverageManager(address(leverageManager)).BASE_RATIO();
-
-        Strategy strategyImplementation = new Strategy();
-        BeaconProxyFactory strategyFactory = new BeaconProxyFactory(address(strategyImplementation), address(this));
-
-        leverageManager.setStrategyTokenFactory(address(strategyFactory));
-
-        strategy = leverageManager.createNewStrategy(
-            Storage.StrategyConfig({
-                lendingAdapter: ILendingAdapter(address(morphoLendingAdapter)),
-                minCollateralRatio: BASE_RATIO,
-                targetCollateralRatio: 2 * BASE_RATIO,
-                maxCollateralRatio: 3 * BASE_RATIO,
-                rebalanceRewardDistributor: IRebalanceRewardDistributor(address(0)),
-                rebalanceWhitelist: IRebalanceWhitelist(address(0))
-            }),
-            "Seamless ETH/USDC 2x leverage token",
-            "ltETH/USDC-2x"
-        );
-    }
-
-    function testFork_setUp() public view override {
-        assertEq(address(leverageManager.getStrategyCollateralAsset(strategy)), address(WETH));
-        assertEq(address(leverageManager.getStrategyDebtAsset(strategy)), address(USDC));
-
-        CollateralRatios memory ratios = leverageManager.getStrategyCollateralRatios(strategy);
-        assertEq(ratios.minCollateralRatio, BASE_RATIO);
-        assertEq(ratios.maxCollateralRatio, 3 * BASE_RATIO);
-        assertEq(ratios.targetCollateralRatio, 2 * BASE_RATIO);
-
-        assertEq(address(leverageManager.getStrategyRebalanceRewardDistributor(strategy)), address(0));
-        assertEq(address(leverageManager.getStrategyRebalanceWhitelist(strategy)), address(0));
-
-        assertEq(leverageManager.getIsLendingAdapterUsed(address(morphoLendingAdapter)), true);
-        assertEq(leverageManager.getStrategyTargetCollateralRatio(strategy), 2 * BASE_RATIO);
-    }
-
+contract LeverageManagerDepositTest is LeverageManagerBase {
     /// @dev In this block price on oracle 3392.292471591441746049801068
     function testFork_deposit_NoFee() public {
         uint256 equityInCollateralAsset = 10 ether;
@@ -169,31 +103,5 @@ contract LeverageManagerTest is IntegrationTestBase {
         // Validate that collateral ratio did not change which means that new deposit follows current collateral ratio and not target
         stateAfter = _getStrategyState();
         assertEq(stateAfter.collateralRatio, stateBefore.collateralRatio);
-    }
-
-    function _deposit(address caller, uint256 equityInCollateralAsset, uint256 collateralToAdd)
-        internal
-        returns (uint256)
-    {
-        deal(address(WETH), caller, collateralToAdd);
-        vm.startPrank(caller);
-        WETH.approve(address(leverageManager), collateralToAdd);
-        (,, uint256 shares,) = leverageManager.deposit(strategy, equityInCollateralAsset, 0);
-        vm.stopPrank();
-
-        return shares;
-    }
-
-    function _getStrategyState() internal view returns (StrategyState memory) {
-        return LeverageManagerHarness(address(leverageManager)).exposed_getStrategyState(strategy);
-    }
-
-    function _convertToAssets(uint256 shares) internal view returns (uint256) {
-        return Math.mulDiv(
-            shares,
-            morphoLendingAdapter.getEquityInCollateralAsset() + 1,
-            strategy.totalSupply() + 1,
-            Math.Rounding.Floor
-        );
     }
 }
---
+++ test/integration/LeverageManager/Withdraw.t.sol
@@ -0,0 +1,101 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.26;
+
+import "forge-std/console.sol";
+
+// Dependency imports
+import {IOracle} from "@morpho-blue/interfaces/IOracle.sol";
+
+// Internal imports
+import {ExternalAction} from "src/types/DataTypes.sol";
+import {LeverageManagerBase} from "./LeverageManagerBase.t.sol";
+import {StrategyState} from "src/types/DataTypes.sol";
+
+contract LeverageManagerWithdrawTest is LeverageManagerBase {
+    /// @dev In this block price on oracle 3392.292471591441746049801068
+    function testFork_withdraw_NoFee() public {
+        uint256 equityInCollateralAsset = 10 ether;
+        uint256 collateralToAdd = 2 * equityInCollateralAsset;
+        _deposit(user, equityInCollateralAsset, collateralToAdd);
+
+        StrategyState memory stateBefore = _getStrategyState();
+
+        uint256 equityToWithdraw = 5 ether;
+        (, uint256 debtToRepay,,) = leverageManager.previewWithdraw(strategy, equityToWithdraw);
+        _withdraw(user, equityToWithdraw, debtToRepay);
+
+        StrategyState memory stateAfter = _getStrategyState();
+
+        // Ensure that collateral ratio is the same. Allow for 1 wei mistake but it must be in favour of strategy
+        assertGe(stateAfter.collateralRatio, stateBefore.collateralRatio);
+        assertLe(stateAfter.collateralRatio, stateBefore.collateralRatio + 1);
+        assertEq(stateAfter.debt, stateBefore.debt - debtToRepay);
+    }
+
+    function testFork_withdraw_MockPrice() public {
+        // Mock ETH price to be 4000 USDC
+        (,, address oracle,,) = morphoLendingAdapter.marketParams();
+        vm.mockCall(address(oracle), abi.encodeWithSelector(IOracle.price.selector), abi.encode(4000e24));
+
+        uint256 equityInCollateralAsset = 10 ether;
+        uint256 collateralToAdd = 2 * equityInCollateralAsset;
+        _deposit(user, equityInCollateralAsset, collateralToAdd);
+
+        StrategyState memory stateBefore = _getStrategyState();
+
+        uint256 equityToWithdraw = 5 ether;
+        (, uint256 debtToRepay,,) = leverageManager.previewWithdraw(strategy, equityToWithdraw);
+        _withdraw(user, equityToWithdraw, debtToRepay);
+
+        StrategyState memory stateAfter = _getStrategyState();
+        uint256 equityInCollateralAssetAfter = morphoLendingAdapter.getEquityInCollateralAsset();
+
+        // Ensure that collateral ratio is the same. Allow for 1 wei mistake but it must be in favour of strategy
+        assertGe(stateAfter.collateralRatio, stateBefore.collateralRatio);
+        assertLe(stateAfter.collateralRatio, stateBefore.collateralRatio + 1);
+
+        // Ensure that after withdraw debt and collateral is 50% of what was initially after deposit
+        assertEq(stateAfter.debt, 20000_000000 - 1); // 2000 USDC, -1 because of rounding
+        assertEq(equityInCollateralAssetAfter, 5 ether);
+    }
+
+    function testFork_withdraw_PriceChangedBetweenWithdraws_CollateralRatioDoesNotChange() public {
+        uint256 equityInCollateralAsset = 10 ether;
+        uint256 collateralToAdd = 2 * equityInCollateralAsset;
+        _deposit(user, equityInCollateralAsset, collateralToAdd);
+
+        // Mock ETH price to be 4000 USDC
+        (,, address oracle,,) = morphoLendingAdapter.marketParams();
+        vm.mockCall(address(oracle), abi.encodeWithSelector(IOracle.price.selector), abi.encode(4000e24));
+
+        StrategyState memory stateBefore = _getStrategyState();
+
+        uint256 equityToWithdraw = 5 ether;
+        (, uint256 debtToRepay,,) = leverageManager.previewWithdraw(strategy, equityToWithdraw);
+        _withdraw(user, equityToWithdraw, debtToRepay);
+
+        StrategyState memory stateAfter = _getStrategyState();
+
+        // Ensure that collateral ratio is the same. Allow for 1 wei mistake but it must be in favour of strategy
+        assertGe(stateAfter.collateralRatio, stateBefore.collateralRatio);
+        assertLe(stateAfter.collateralRatio, stateBefore.collateralRatio + 1);
+    }
+
+    function testFork_withdraw_withFee() public {
+        leverageManager.setStrategyActionFee(strategy, ExternalAction.Withdraw, 10_00); // 10%
+
+        uint256 equityInCollateralAsset = 10 ether;
+        uint256 collateralToAdd = 2 * equityInCollateralAsset;
+        _deposit(user, equityInCollateralAsset, collateralToAdd);
+
+        uint256 equityInCollateralAssetAfterDeposit = morphoLendingAdapter.getEquityInCollateralAsset();
+
+        // Withdraw 50% of equity
+        uint256 equityToWithdraw = equityInCollateralAssetAfterDeposit / 2;
+        (, uint256 debtToRepay,,) = leverageManager.previewWithdraw(strategy, equityToWithdraw);
+        _withdraw(user, equityToWithdraw, debtToRepay);
+
+        // Lower or equal because or rounding, theoretically perfect would be 4.5 ether
+        assertEq(strategy.balanceOf(user), 4.5 ether + 1); // +1 because of rounding, equityToWithdraw is rounded down so shares to burn will also be a bit lower
+    }
+}
