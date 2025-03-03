0b01d93abc38020634f997b0bb9da56000de50cb
chad-js <chadflemies@gmail.com>
2025-03-03 16:31:34 -0500

    test: move constants to base contract

--- test/integration/LeverageRouter/LeverageRouterBase.t.sol
+++ test/integration/LeverageRouter/LeverageRouterBase.t.sol
@@ -23,6 +23,12 @@ import {IntegrationTestBase} from "../IntegrationTestBase.t.sol";
 import {CollateralRatios} from "src/types/DataTypes.sol";
 
 contract LeverageRouterBase is IntegrationTestBase {
+    address public constant AERODROME_ROUTER = 0xcF77a3Ba9A5CA399B7c97c74d54e5b1Beb874E43;
+    address public constant AERODROME_SLIPSTREAM_ROUTER = 0xBE6D8f0d05cC4be24d5167a3eF062215bE6D18a5;
+    address public constant AERODROME_POOL_FACTORY = 0x420DD381b31aEf6683db6B902084cB0FFECe40Da;
+    address public constant UNISWAP_V2_ROUTER02 = 0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24;
+    address public constant UNISWAP_SWAP_ROUTER02 = 0x2626664c2603336E57B271c5C0b26F421741e481;
+
     ILeverageRouter public leverageRouter;
     ISwapAdapter public swapAdapter;
 
