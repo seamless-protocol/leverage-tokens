1528d991e00e0f1c358457af6efce0ee7a966d00
chad-js <chadflemies@gmail.com>
2025-03-20 14:11:36 -0400

    test: fix assume

--- test/unit/Strategy/Permit.t.sol
+++ test/unit/Strategy/Permit.t.sol
@@ -14,11 +14,11 @@ contract PermitTest is StrategyBaseTest {
 
     /// @dev Sanity test to ensure that token supports permit
     function testFuzz_permit(PermitLib.Permit memory permit, uint256 privateKey, uint128 blockTimestamp) public {
+        vm.assume(permit.spender != address(0));
         vm.warp(blockTimestamp);
 
         privateKey = bound(privateKey, 1, type(uint32).max);
         permit.owner = vm.addr(privateKey);
-        vm.assume(permit.owner != address(0));
 
         permit.deadline = bound(permit.deadline, block.timestamp, type(uint256).max);
         permit.nonce = strategyToken.nonces(permit.owner);
