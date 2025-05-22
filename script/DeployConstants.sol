3f2b34aeca29947454f5c22d49df500db85e1824
Danilo Kitanovic <116795362+kitanovicd@users.noreply.github.com>
2025-05-22 08:36:59 -0700

    feat: deployment scripts (#213)

---
+++ script/DeployConstants.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+library DeployConstants {
+    address public constant MORPHO = 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb;
+    address public constant SEAMLESS_TIMELOCK_SHORT = 0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee;
+    address public constant SEAMLESS_TREASURY = 0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee;
+
+    // TODO: Update this after deployment
+    address public constant LEVERAGE_MANAGER = address(0);
+    address public constant LENDING_ADAPTER_FACTORY = address(0);
+}
