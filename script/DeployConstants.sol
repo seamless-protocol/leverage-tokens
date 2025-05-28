0be04317a3232db1d432f7b2b1d7fda6c1eb5467
fredwes <6827305+fredwes@users.noreply.github.com>
2025-05-28 11:56:27 -0700

    feat: create WEETH-WETH-17x (#223)

--- script/DeployConstants.sol
+++ script/DeployConstants.sol
@@ -6,7 +6,11 @@ library DeployConstants {
     address public constant SEAMLESS_TIMELOCK_SHORT = 0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee;
     address public constant SEAMLESS_TREASURY = 0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee;
 
+    address public constant WETH = 0x4200000000000000000000000000000000000006;
+    address public constant WEETH = 0x04C0599Ae5A44757c0af6F9eC3b93da8976c150A;
+
     // TODO: Update this after deployment
     address public constant LEVERAGE_MANAGER = address(0);
     address public constant LENDING_ADAPTER_FACTORY = address(0);
+    address public constant ETHERFI_LEVERAGE_ROUTER = address(0);
 }
