1b525831645c94d8134901c2e5244d29a5ccbeff
chad-js <chadflemies@gmail.com>
2025-05-29 18:32:25 -0400

    update scripts

--- script/DeployConstants.sol
+++ script/DeployConstants.sol
@@ -6,6 +6,7 @@ library DeployConstants {
     address public constant SEAMLESS_TIMELOCK_SHORT = 0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee;
     address public constant SEAMLESS_TREASURY = 0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee;
     address public constant ETHERFI_L2_MODE_SYNC_POOL = 0xc38e046dFDAdf15f7F56853674242888301208a5;
+    address public constant ETHERFI_ETH_IDENTIFIER = 0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE;
 
     address public constant WETH = 0x4200000000000000000000000000000000000006;
     address public constant WEETH = 0x04C0599Ae5A44757c0af6F9eC3b93da8976c150A;
@@ -13,5 +14,5 @@ library DeployConstants {
     // TODO: Update this after deployment
     address public constant LEVERAGE_MANAGER = address(0);
     address public constant LENDING_ADAPTER_FACTORY = address(0);
-    address public constant ETHERFI_LEVERAGE_ROUTER = address(0);
+    address public constant LEVERAGE_ROUTER = address(0);
 }
