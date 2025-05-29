340639f65bea733d5b041e7686b2500033812c7e
Danilo Kitanovic <116795362+kitanovicd@users.noreply.github.com>
2025-05-29 17:53:35 +0200

    fix: script update (#225)

--- script/DeployConstants.sol
+++ script/DeployConstants.sol
@@ -5,6 +5,7 @@ library DeployConstants {
     address public constant MORPHO = 0xBBBBBbbBBb9cC5e90e3b3Af64bdAF62C37EEFFCb;
     address public constant SEAMLESS_TIMELOCK_SHORT = 0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee;
     address public constant SEAMLESS_TREASURY = 0x639d2dD24304aC2e6A691d8c1cFf4a2665925fee;
+    address public constant ETHERFI_L2_MODE_SYNC_POOL = 0xc38e046dFDAdf15f7F56853674242888301208a5;
 
     address public constant WETH = 0x4200000000000000000000000000000000000006;
     address public constant WEETH = 0x04C0599Ae5A44757c0af6F9eC3b93da8976c150A;
