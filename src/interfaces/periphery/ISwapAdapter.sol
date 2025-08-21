1f17a60b4bd1e3e7190be90f531887c3d72b5a4d
chad-js <chadflemies@gmail.com>
2025-08-21 12:06:59 -0400

    update natspec

--- src/interfaces/periphery/ISwapAdapter.sol
+++ src/interfaces/periphery/ISwapAdapter.sol
@@ -78,14 +78,17 @@ interface ISwapAdapter {
     /// @notice Error thrown when the number of fees is invalid
     error InvalidNumFees();
 
-    /// @notice Execute one approval (optional), then one arbitrary external swap call. All outputToken is sent to the
+    /// @notice Execute an approval (optional), then an arbitrary external swap call. All outputToken is sent to the
     /// recipient. Any leftover inputToken is sent to the sender.
-    /// @param approval The approval to set before the call (set token=address(0) to skip).
+
+    /// @notice Execute an arbitrary external swap call. All outputToken is sent to the recipient. Any leftover inputToken is sent to the sender.
+    /// Note: If the inputToken is the same as the outputToken, any leftover inputToken is sent to the recipient instead of the sender.
+    /// @param approval The approval to set before the call (set token=address(0) to skip). e.g. approving a DEX to pull the inputToken from the SwapAdapter.
     /// @param call External call to perform (DEX/router).
     /// @param inputToken Input token for the swap (address(0) = ETH).
     /// @param outputToken Output token for the swap (address(0) = ETH).
-    /// @param inputAmount Amount of input token for the swap. Note: If the sender transferred the required amount of
-    /// input token to this contract already, this can be set to zero.
+    /// @param inputAmount Amount of input token for the swap, which is tranferred from the sender to the SwapAdapter.
+    /// Note: If the sender transferred the required amount of input token to this contract already, this can be set to zero.
     /// @param recipient Where to send the output and any leftover ETH.
     /// @return result Return data of the external call.
     function execute(
